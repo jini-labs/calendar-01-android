169e81372490b1ced7d45186df5b3858ff132e89
goodjini <jinijoa0@gmail.com>
2017-01-31 00:58:36 +0900

    期間計算の履歴表示
    　listview, costum adapter, scroll.

--- app/src/main/java/com/doublesibi/utils/calc/datecalculator/DurationHistActivity.java
+++ app/src/main/java/com/doublesibi/utils/calc/datecalculator/DurationHistActivity.java
@@ -1,45 +1,126 @@
 package com.doublesibi.utils.calc.datecalculator;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.doublesibi.utils.calc.datecalculator.hist.DurationHistItem;
 import com.doublesibi.utils.calc.datecalculator.hist.DurationItemOpenHelper;
 
+import java.util.ArrayList;
 
-public class DurationHistActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class DurationHistActivity extends AppCompatActivity {
+    private final String LOGTAG = "DayCalc";
+
+    // 1ページ辺りの項目数
+    Integer per_page = 10;
+    private int recordCount = 0;
+    private int selectKey = 0;
+
+    // フッターのプログレスバー（クルクル）
+    View mFooter;
+
+    // 予報表示用リストビューのアダプター
+    //ArrayAdapter<String> adapter;
+    private DurationHistAdaptor adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_duration_hist);
 
-        findViewById(R.id.btnDuraReturn).setOnClickListener(this);
+        // リスト用のアダプターを準備
+        //adapter = new ArrayAdapter<String>(this, R.layout.duration_hist_listview_item);
+        adapter = new DurationHistAdaptor(DurationHistActivity.this);
 
+                // アダプターにアイテムを追加します
+//        for (int i = 0; i < per_page; i++) {
+//            adapter.add("リストビュー：" + i);
+//        }
+        getDurationHistData(selectKey);
+
+        // リストビューへ紐付け
+        ListView listview = (ListView)findViewById(R.id.listView);
+
+        // リストビューにアダプターを設定します
+        listview.setAdapter(adapter);
+
+        // リストビューにフッターを追加
+        listview.addFooterView(getFooter());
+
+        // スクロールのリスナー
+        listview.setOnScrollListener(new AbsListView.OnScrollListener() {
+
+            // スクロール中の処理
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+                // 最初とスクロール完了したとき
+                if ((totalItemCount - visibleItemCount) == firstVisibleItem) {
+
+                    // アイテムの数 フッター分の1を引く
+                    Integer ItemCount = totalItemCount - 1;
+
+                    // アダプターにアイテムを追加します
+//                    for (int i = ItemCount; i < (ItemCount + per_page); i++) {
+//                        adapter.add("リストビュー：" + i);
+//                    }
+                    getDurationHistData(selectKey);
+                }
+            }
+
+            // ListViewがスクロール中かどうか状態を返すメソッドです
+            @Override
+            public void onScrollStateChanged(AbsListView arg0, int arg1) {
+            }
+        });
+    }
+
+    private View getFooter() {
+        if (mFooter == null) {
+            mFooter = getLayoutInflater().inflate(R.layout.footer_duration_hist, null);
+        }
+        return mFooter;
+    }
+
+    private void getDurationHistData(int startDate) {
         DurationItemOpenHelper helper = new DurationItemOpenHelper(this);
         SQLiteDatabase db = helper.getReadableDatabase();
-
+        String[] columns ={"stDate",
+                            "enDate",
+                            "days",
+                            "weeks",
+                            "weekdays",
+                            "months",
+                            "monthdays",
+                            "years",
+                            "yearmonths",
+                            "yeardays" };
         // queryメソッドの実行例
-        Cursor c = db.query("DateDuration", new String[] {  "stDate",
-                                                            "enDate",
-                                                            "days",
-                                                            "weeks",
-                                                            "weekdays",
-                                                            "months",
-                                                            "monthdays",
-                                                            "years",
-                                                            "yearmonths",
-                                                            "yeardays" }, null,
-        null, null, null, null);
+        Cursor c = db.query("DateDuration", columns,
+                            "stDate>?", new String[] {""+startDate},
+                            null,
+                            null,
+                            "stDate",
+                            ""+this.per_page);
 
-        TextView textView = (TextView)findViewById(R.id.textView);
         String text = null;
         boolean mov = c.moveToFirst();
         while (mov) {
-            text = "[stDate:"      + c.getString(0) +
+            selectKey = Integer.parseInt(c.getString(0));
+
+            text = "[stDate:"      + selectKey +
                     ", enDate:"     + c.getString(1) +
                     ", days:"       + c.getString(2) +
                     ", weeks:"      + c.getString(3) +
@@ -50,20 +131,105 @@ public class DurationHistActivity extends AppCompatActivity implements View.OnCl
                     ", yearmonths:" + c.getString(8) +
                     ", yeardays:"   + c.getString(9) + "]";
 
-            if (textView.getText().toString().length() > 0)
-                text = textView.getText().toString() + "\n" + text;
 
-            textView.setText(text);
+            recordCount ++;
+            DurationHistItem item = new DurationHistItem();
+            item.setStartDate(c.getString(0));
+            item.setEndDate(c.getString(1));
+            item.setDurDays(c.getString(2) + " 日");
+
+            if (StrToInt(c.getString(3)) > 0 ) {
+                item.setDurWeeksDays(c.getString(3) +
+                        " 週, ");
+                if (StrToInt(c.getString(4)) > 0 ) {
+                    item.setDurWeeksDays(item.getDurWeeksDays() +
+                            c.getString(4) +
+                            " 日");
+                }
+            }
+            if (StrToInt(c.getString(5)) > 0 ) {
+                item.setDurMonthsDays(c.getString(5) +
+                        "カ月, ");
+                if (StrToInt(c.getString(6)) > 0 ) {
+                    item.setDurMonthsDays(item.getDurMonthsDays() +
+                            c.getString(6) +
+                            " 日");
+                }
+
+            }
+            if (StrToInt(c.getString(7)) > 0 ) {
+                item.setDurYearsMonthsDays(c.getString(7)
+                        + " 年, ");
+                if (StrToInt(c.getString(8)) > 0) {
+                    item.setDurYearsMonthsDays(item.getDurYearsMonthsDays() +
+                            c.getString(8) +
+                            " カ月, ");
+                }
+                if (StrToInt(c.getString(9)) > 0) {
+                    item.setDurYearsMonthsDays(item.getDurYearsMonthsDays() +
+                            c.getString(9) +
+                            " 日");
+                }
+            }
+
+            Log.d(LOGTAG, item.toString());
+            adapter.setItems(item);
             mov = c.moveToNext();
         }
         c.close();
         db.close();
     }
 
-    @Override
-    public void onClick(View v) {
-        if(v.getId() == R.id.btnDuraReturn) {
-            finish();
+    private int StrToInt(String str) {
+        if (str == null)
+            return 0;
+        if (str.trim().length() == 0)
+            return 0;
+        return Integer.parseInt(str.trim());
+    }
+    public class DurationHistAdaptor extends BaseAdapter {
+        Context context;
+        LayoutInflater layoutInflater = null;
+        ArrayList<DurationHistItem> items;
+
+        public DurationHistAdaptor(Context context) {
+            this.context = context;
+            this.layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            this.items = new ArrayList<>();
+        }
+
+        public void setItems(DurationHistItem item) {
+            this.items.add(item);
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = layoutInflater.inflate(R.layout.duration_hist_listview_item, parent, false);
+
+            ((TextView)convertView.findViewById(R.id.durationStart)).setText(items.get(position).getStartDate());
+            ((TextView)convertView.findViewById(R.id.durationEnd)).setText(items.get(position).getEndDate());
+
+            ((TextView)convertView.findViewById(R.id.durationDays)).setText(items.get(position).getDurDays());
+            ((TextView)convertView.findViewById(R.id.durationWeeksDays)).setText(items.get(position).getDurWeeksDays());
+            ((TextView)convertView.findViewById(R.id.durationMonthsDays)).setText(items.get(position).getDurMonthsDays());
+            ((TextView)convertView.findViewById(R.id.durationYearsMonthsDays)).setText(items.get(position).getDurYearsMonthsDays());
+
+            return convertView;
         }
     }
 }
---
+++ app/src/main/java/com/doublesibi/utils/calc/datecalculator/hist/DurationHistItem.java
@@ -0,0 +1,83 @@
+package com.doublesibi.utils.calc.datecalculator.hist;
+
+/**
+ * Created by hunajini on 2017/01/30.
+ */
+
+public class DurationHistItem {
+    String startDate;
+    String endDate;
+    String durDays;
+    String durWeeksDays;
+    String durMonthsDays;
+    String durYearsMonthsDays;
+
+    public DurationHistItem() {
+        this.startDate = "";
+        this.endDate = "";
+        this.durDays = "";
+        this.durWeeksDays = "";
+        this.durMonthsDays = "";
+        this.durYearsMonthsDays = "";
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDurDays() {
+        return durDays;
+    }
+
+    public void setDurDays(String durDays) {
+        this.durDays = durDays;
+    }
+
+    public String getDurWeeksDays() {
+        return durWeeksDays;
+    }
+
+    public void setDurWeeksDays(String durWeeksDays) {
+        this.durWeeksDays = durWeeksDays;
+    }
+
+    public String getDurMonthsDays() {
+        return durMonthsDays;
+    }
+
+    public void setDurMonthsDays(String durMonthsDays) {
+        this.durMonthsDays = durMonthsDays;
+    }
+
+    public String getDurYearsMonthsDays() {
+        return durYearsMonthsDays;
+    }
+
+    public void setDurYearsMonthsDays(String durYearsMonthsDays) {
+        this.durYearsMonthsDays = durYearsMonthsDays;
+    }
+
+    @Override
+    public String toString() {
+        return "DurationHistItem{" +
+                "startDate='" + startDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                ", durDays='" + durDays + '\'' +
+                ", durWeeksDays='" + durWeeksDays + '\'' +
+                ", durMonthsDays='" + durMonthsDays + '\'' +
+                ", durYearsMonthsDays='" + durYearsMonthsDays + '\'' +
+                '}';
+    }
+}
