f68c86b826e8067591ca524a04660fcf7a0f01aa
goodjini <jinijoa0@gmail.com>
2017-10-15 16:54:45 +0900

    Bug fix

--- app/src/main/java/com/doublesibi/utils/calc/datecalculator/CalculatorFragment.java
+++ app/src/main/java/com/doublesibi/utils/calc/datecalculator/CalculatorFragment.java
@@ -11,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.StringTokenizer;
@@ -24,7 +23,7 @@ public class CalculatorFragment extends Fragment implements View.OnClickListener
 
     private static final double CONSUMPTION_TAX_RATE = 0.08;
 
-    private EditText inputedStatement;
+    private EditText inputedStatement2;
     private TextView currInputTxt;
     private ArrayList<Button> arrayNumBtnList;
 
@@ -32,9 +31,14 @@ public class CalculatorFragment extends Fragment implements View.OnClickListener
 
     private Button btnDot;
     private Button btnCalcPlus, btnCalcMinus, btnCalcDivide, btnCalcMultiply;
-    private Button btnCalcTax, btnCalcResult;
+    private Button btnCalcTaxInc, btnCalcTaxExc, btnCalcResult;
 
-    boolean bDotClicked = false;
+    private boolean bDotClicked = false;
+    private boolean bCalculated = false;
+    private int nCalculatable = 0;
+
+    private String strTaxCalculate;
+    private boolean bTaxCalculated;
 
     public CalculatorFragment() {
         // Required empty public constructor
@@ -46,7 +50,7 @@ public class CalculatorFragment extends Fragment implements View.OnClickListener
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_calculator, container, false);
-        inputedStatement = (EditText)view.findViewById(R.id.inputedStatement);
+        inputedStatement2 = (EditText)view.findViewById(R.id.inputedStatement2);
         currInputTxt = (TextView)view.findViewById(R.id.currInput);
         arrayNumBtnList = new ArrayList<>();
 
@@ -57,10 +61,10 @@ public class CalculatorFragment extends Fragment implements View.OnClickListener
 
     @Override
     public void onClick(View v) {
-        String strTmp1, strTmp2, strTmp3, strTmp4;
+        String strTmp1, strTmp2;
         String strRtn = null;
-        int nTmp1, nTmp2;
-        double dTmp1, dTmp2, dTmp3;
+        int nTmp1;
+        double dTmp1, dTmp2;
         int clickedId = 0;
         Button numBtn;
         Button operBtn;
@@ -82,7 +86,7 @@ public class CalculatorFragment extends Fragment implements View.OnClickListener
                 break;
             case R.id._0:
             case R.id._00:
-                if (currInputTxt.getText().length() > 0) {
+                if (currInputTxt.getText().length() > 0 || bDotClicked) {
                     numBtn = (Button) v.findViewById(clickedId);
                     numBtn.getText();
                     currInputTxt.setText(currInputTxt.getText().toString() + numBtn.getText().toString());
@@ -90,35 +94,27 @@ public class CalculatorFragment extends Fragment implements View.OnClickListener
                 break;
             case R.id._dot:
                 if (!bDotClicked) {
-                    numBtn = (Button) v.findViewById(clickedId);
-                    numBtn.getText();
-                    currInputTxt.setText(currInputTxt.getText().toString() + numBtn.getText().toString());
-                    bDotClicked = true;
+                    currInputTxt.setText("0");
                 }
+                currInputTxt.setText(currInputTxt.getText().toString() + ".");
+                bDotClicked = true;
                 break;
             case R.id.clear:
-                inputedStatement.setText("");
+                inputedStatement2.setText("");
                 currInputTxt.setText("");
-                if (bDotClicked)
-                    bDotClicked = !bDotClicked;
-
-                for (Button btnItem : arrayNumBtnList) {
-                    btnItem.setClickable(true);
-                }
-
-                for (Button btnItem : arrayNumBtnList) {
-                    btnItem.setClickable(true);
-                }
-
-                disalbeButton(v, true, 10,10,10);
+                bDotClicked = false;
+                bCalculated = false;
                 break;
             case R.id.delete:
                 if (currInputTxt.getText().length() > 0) {
+                    if (bDotClicked || currInputTxt.getText().toString().equals("0.")) {
+                        currInputTxt.setText("");
+                        bDotClicked = false;
+                        break;
+                    }
+
                     strTmp1 = currInputTxt.getText().toString();
                     nTmp1 = strTmp1.length();
-                    if (strTmp1.charAt(nTmp1 - 1) == '.') {
-                        bDotClicked = !bDotClicked;
-                    }
                     strTmp2 = strTmp1.substring(0, nTmp1 - 1);
 
                     currInputTxt.setText(strTmp2);
@@ -128,84 +124,91 @@ public class CalculatorFragment extends Fragment implements View.OnClickListener
             case R.id.calcMinus:
             case R.id.calcDivide:
             case R.id.calcMultiply:
+                if (bCalculated) {
+                    inputedStatement2.setText("");
+                    bCalculated=false;
+                    nCalculatable=0;
+                }
+                operBtn = (Button) v.findViewById(clickedId);
                 if (currInputTxt.getText().length() > 0) {
-                    operBtn = (Button) v.findViewById(clickedId);
-
-                    Log.d(LOG_TAG, "clicked operation button : " + operBtn.getText().toString());
-
-                    if (inputedStatement.getText().length() > 0) {
-                        inputedStatement.setText(inputedStatement.getText().toString()
-                                + currInputTxt.getText().toString()
-                                + operBtn.getText().toString());
-                    } else {
-                        inputedStatement.setText(currInputTxt.getText().toString() + operBtn.getText().toString());
-                    }
-
-                    //Log.d(LOG_TAG, "inputedStatement : " + inputedStatement.getText());
+                    String tmp = inputedStatement2.getText().toString();
+                    inputedStatement2.setText(tmp + currInputTxt.getText().toString() + operBtn.getText().toString().trim());
+                    nCalculatable ++;
                     currInputTxt.setText("");
                 }
                 break;
-            case R.id.calcTax:
-                if (btnCalcTax.getText().toString().equals("税込")) {
-                    strTmp1 = currInputTxt.getText().toString();
-                    dTmp1 = Integer.parseInt(strTmp1) * (1 + CONSUMPTION_TAX_RATE);
+            case R.id.calcTaxInc:
+                if (currInputTxt.getText().length() > 0) {
+                    strTaxCalculate = currInputTxt.getText().toString();
+                    dTmp1 = Integer.parseInt(currInputTxt.getText().toString()) * (1 + CONSUMPTION_TAX_RATE);
+                    dTmp1 = Math.floor(dTmp1);
                     currInputTxt.setText("" + (int) dTmp1);
 
-                    btnCalcTax.setText("税抜");
-                } else {
-                    strTmp1 = currInputTxt.getText().toString();
-                    dTmp1 = Integer.parseInt(strTmp1) / (1 + CONSUMPTION_TAX_RATE);
-                    currInputTxt.setText("" + (int) dTmp1);
+                    bTaxCalculated = true;
+                }
+                break;
+            case R.id.calcTaxExc:
+                if (currInputTxt.getText().length() > 0) {
+                    strTaxCalculate = currInputTxt.getText().toString();
+                    dTmp1 = Integer.parseInt(currInputTxt.getText().toString()) / (1 + CONSUMPTION_TAX_RATE);
+                    currInputTxt.setText("" + (int) (dTmp1 + 0.5));
 
-                    btnCalcTax.setText("税込");
+                    bTaxCalculated = true;
                 }
                 break;
             case R.id.calcResult:
-                if (inputedStatement.getText().length() > 0) {
+                if (nCalculatable == 0) {
+                    break;
+                } else if (nCalculatable == 1) {
                     if (currInputTxt.getText().length() > 0) {
-                        inputedStatement.setText(inputedStatement.getText().toString() + currInputTxt.getText().toString() + "=");
+                        strTmp1 = inputedStatement2.getText().toString();
+                        inputedStatement2.setText(strTmp1 + currInputTxt.getText().toString() + "=");
+
+                        currInputTxt.setText("");
+                        nCalculatable++;
                     } else {
-                        strTmp1 = inputedStatement.getText().toString();
-                        strTmp2 = strTmp1.substring(0, strTmp1.length()-1);
-                        strTmp2 = strTmp2 + "=";
-                        inputedStatement.setText(strTmp2);
+                        break;
+                    }
+                }
+
+                if (nCalculatable > 1) {
+                    if (currInputTxt.getText().length() > 0) {
+                        strTmp1 = inputedStatement2.getText().toString();
+                        inputedStatement2.setText(strTmp1 + currInputTxt.getText().toString() + "=");
+                        currInputTxt.setText("");
+                        nCalculatable++;
                     }
 
-                    disalbeButton(v, false, 200,196,196);
+                    strTmp1 = inputedStatement2.getText().toString();
+                    strTmp2 = strTmp1.substring(0, strTmp1.length()-1);
+                    inputedStatement2.setText(strTmp2 + "=");
+
                     // ...
-                    strRtn = calculate(inputedStatement.getText().toString());
+                    strRtn = calculate(inputedStatement2.getText().toString());
                     // ...
 
                     dTmp1 = Double.parseDouble(strRtn);
                     dTmp2 = dTmp1 % 1;
                     if (dTmp2 == 0.0) {
-                        inputedStatement.setText(inputedStatement.getText().toString() + "  " + (int)dTmp1);
+                        inputedStatement2.setText(inputedStatement2.getText().toString() + "  " + (int)dTmp1);
                     } else {
-                        inputedStatement.setText(inputedStatement.getText().toString() + "  " + dTmp1);
+                        inputedStatement2.setText(inputedStatement2.getText().toString() + "  " + dTmp1);
                     }
 
-                    currInputTxt.setText("");
-
-                } else {
-                    if (currInputTxt.getText().length() > 0) {
-                    } else {
-                    }
+                    bCalculated = true;
+                    nCalculatable = 0;
                 }
-
                 break;
         }
-
-        //Log.d(LOG_TAG, "Input Text : " + currInputTxt.getText().toString() + ", inputedStat:" + inputedStatement.getText().toString());
     }
 
     private String calculate(String strExprssion) {
         String strTmp1, strTmp2, strTmp3, strTmp4;
-        int nTmp1, nTmp2;
-        double dTmp1, dTmp2, dTmp3;
+        double dTmp1;
         String atoken = null;
         ArrayList<String> numsArray = new ArrayList<>();
         ArrayList<String> opersArray = new ArrayList<>();
-        StringTokenizer token = new StringTokenizer(strExprssion, "+-x/=");
+        StringTokenizer token = new StringTokenizer(strExprssion, "+-x÷=");
         while(token.hasMoreTokens()) {
             atoken = token.nextToken();
             numsArray.add(atoken);
@@ -217,14 +220,12 @@ public class CalculatorFragment extends Fragment implements View.OnClickListener
             opersArray.add(atoken);
         }
 
-        Log.d(LOG_TAG, "opersArray.size:" + opersArray.size() + ", numsArray.size:" + numsArray.size());
-
         while(opersArray.size() > 1) {
 
             strTmp1 = opersArray.get(0);
             if (strTmp1.equals("=")) {
                 break;
-            } else if (strTmp1.equals("x") || strTmp1.equals("/")) {
+            } else if (strTmp1.equals("x") || strTmp1.equals("÷")) {
                 strTmp2 = numsArray.get(0);
                 strTmp3 = numsArray.get(1);
 
@@ -244,7 +245,7 @@ public class CalculatorFragment extends Fragment implements View.OnClickListener
                     opersArray.remove(0);
                     numsArray.remove(0);
                     numsArray.set(0, String.valueOf(dTmp1));
-                } else if (strTmp2.equals("x") || strTmp2.equals("/")) {
+                } else if (strTmp2.equals("x") || strTmp2.equals("÷")) {
                     strTmp3 = numsArray.get(1);
                     strTmp4 = numsArray.get(2);
 
@@ -277,36 +278,16 @@ public class CalculatorFragment extends Fragment implements View.OnClickListener
             return left - right;
         } else if (oper.equals("x")) {
             return left * right;
-        } else if (oper.equals("/")) {
+        } else if (oper.equals("÷")) {
             return left / right;
         } else {
             return 0.0;
         }
     }
-    void disalbeButton(View v, boolean flag, int r, int g, int b) {
 
-        for (Button btnItem : arrayNumBtnList) {
-            btnItem.setClickable(flag);
-            btnItem.setTextColor(Color.rgb(r, g, b));
-        }
+    private double calcTax(double orgValue, double taxRate) {
 
-        for (Button btnItem : arrayNumBtnList) {
-            btnItem.setClickable(flag);
-            btnItem.setTextColor(Color.rgb(r,g,b));
-        }
-
-        btnDelete.setClickable(flag);
-        btnDot.setClickable(flag);
-
-        btnDelete.setTextColor(Color.rgb(r,g,b));
-        btnDot.setTextColor(Color.rgb(r, g, b));
-
-        btnCalcPlus.setTextColor(Color.rgb(r,g,b));
-        btnCalcMinus.setTextColor(Color.rgb(r,g,b));
-        btnCalcDivide.setTextColor(Color.rgb(r,g,b));
-        btnCalcMultiply.setTextColor(Color.rgb(r,g,b));
-
-        btnCalcResult.setTextColor(Color.rgb(r,g,b));
+        return orgValue * this.CONSUMPTION_TAX_RATE;
     }
 
     void setButtonId(View v) {
@@ -331,14 +312,16 @@ public class CalculatorFragment extends Fragment implements View.OnClickListener
         btnCalcMinus = (Button)v.findViewById(R.id.calcMinus);
         btnCalcDivide = (Button)v.findViewById(R.id.calcDivide);
         btnCalcMultiply = (Button)v.findViewById(R.id.calcMultiply);
-        btnCalcTax = (Button)v.findViewById(R.id.calcTax);
+        btnCalcTaxInc = (Button)v.findViewById(R.id.calcTaxInc);
+        btnCalcTaxExc= (Button)v.findViewById(R.id.calcTaxExc);
 
         btnDot.setOnClickListener(this);
         btnCalcPlus.setOnClickListener(this);
         btnCalcMinus.setOnClickListener(this);
         btnCalcDivide.setOnClickListener(this);
         btnCalcMultiply.setOnClickListener(this);
-        btnCalcTax.setOnClickListener(this);
+        btnCalcTaxInc.setOnClickListener(this);
+        btnCalcTaxExc.setOnClickListener(this);
 
         btnDelete = (Button)v.findViewById(R.id.delete);
         btnClear = (Button)v.findViewById(R.id.clear);
